a9467cee3aecf7d306a9c96f7ea14c06595c5f96
axk5209 <arhamkothari@utexas.edu>
2020-07-23 23:27:55 -0500

    Integrated static build

---
+++ build/precache-manifest.94757a8ece6b8e0a445fc7fcac254acb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "def3f2295b53fcffe505abcb551195f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbd095dc2e462a7b0c1b",
+    "url": "/static/js/2.2ece41b9.chunk.js"
+  },
+  {
+    "revision": "3154232bc28eccce26da871c6ca8aff8",
+    "url": "/static/js/2.2ece41b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74583a7044756b2b1e37",
+    "url": "/static/js/main.006168e3.chunk.js"
+  },
+  {
+    "revision": "9b36dbe927e5c9e02330",
+    "url": "/static/js/runtime-main.29d026f8.js"
+  },
+  {
+    "revision": "582dde991bea737f5c7287fe6b508bce",
+    "url": "/static/media/chefHat.582dde99.jpg"
+  }
+]);
